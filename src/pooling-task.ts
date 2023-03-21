55842f0bcb0b0cf909a4c04d897c85bbd0b155b6
lucas.dornelas <lucas.dornelas@zukk.com.br>
2023-03-21 13:23:15 -0300

    first commit

---
+++ src/pooling-task.ts
@@ -0,0 +1,263 @@
+import {
+	Observable,
+	Subject,
+	Subscription,
+	asyncScheduler,
+	defer,
+	finalize,
+	from,
+	mergeMap,
+	of,
+	retry,
+	subscribeOn,
+	takeLast,
+	takeUntil,
+	throwError,
+} from 'rxjs';
+
+export enum JobStateType {
+	Sending,
+	Created,
+	Finished,
+	Destroyed,
+}
+
+export enum JobStatusType {
+	Running,
+	Success,
+	Failed,
+}
+
+export interface JobStatusInfo<T = any> {
+	status: JobStatusType;
+	error?: any;
+	data?: T;
+}
+
+type AsyncReturn<T> = T | Observable<T> | Promise<T>;
+
+function resolveAsyncReturn<T>(
+	res: AsyncReturn<T>,
+	shouldThrowError = false
+): Observable<T> {
+	if (res instanceof Promise) {
+		res = from(res);
+	}
+
+	if (!(res instanceof Observable)) {
+		res = of(res);
+	}
+
+	if (shouldThrowError) {
+		res = res.pipe(
+			mergeMap((x) => {
+				return throwError(() => x);
+			})
+		);
+	}
+
+	return res;
+}
+
+export interface IPoolingOptions<T, I, R = JobStatusInfo<I>> {
+	jobFactory?: () => AsyncReturn<T>;
+	checkJobFn: (jobData: T) => AsyncReturn<JobStatusInfo<I>>;
+	doFinished?: (info: JobStatusInfo<I>, jobData: T) => AsyncReturn<R>;
+	doFailed?: (response: any, jobData: T) => AsyncReturn<any>;
+	retryTimes?: number;
+	initialCheckInterval?: number;
+	checkInterval?: number;
+}
+
+export class RxPoolingTask<T, I, R = I> {
+	private _state: JobStateType = null;
+
+	private set state(value) {
+		if (this._state === value) {
+			return;
+		}
+
+		this._state = value;
+		this._jobStateSubject.next(value);
+	}
+
+	get state() {
+		return this._state;
+	}
+
+	private _jobStatus: JobStatusInfo<I> = null;
+
+	get jobStatus() {
+		return this._jobStatus;
+	}
+
+	private _jobStateSubject = new Subject<JobStateType>();
+	private _jobStatusSubject = new Subject<JobStatusInfo<I>>();
+
+	get onJobEvent() {
+		return this._jobStatusSubject.asObservable();
+	}
+
+	get onJobStatus() {
+		return this._jobStatusSubject.asObservable();
+	}
+
+	get started() {
+		return this.state !== null;
+	}
+
+	get created() {
+		return this.state === JobStateType.Created;
+	}
+
+	get finished() {
+		return this.state === JobStateType.Finished;
+	}
+
+	get destroyed() {
+		return this.state === JobStateType.Destroyed;
+	}
+
+	private _destroySubject = new Subject<void>();
+
+	constructor(private options: IPoolingOptions<T, I, R>) {}
+
+	submit(): Observable<R> {
+		this._checkDestroyed();
+
+		if (this.state !== null) {
+			throw new Error('Task has started');
+		}
+
+		this.state = JobStateType.Sending;
+		return defer(() => {
+			this._checkDestroyed();
+
+			if (this.state !== null && this.state !== JobStateType.Sending) {
+				throw new Error('Task has started');
+			}
+
+			return resolveAsyncReturn(this.options.jobFactory?.()).pipe(
+				mergeMap((jobData) => {
+					this.state = JobStateType.Created;
+
+					const checkInterval = this.options.checkInterval ?? 1000;
+
+					return this._observeJob(
+						jobData,
+						this.options.initialCheckInterval ?? checkInterval,
+						checkInterval
+					).pipe(
+						takeLast(1),
+						mergeMap((jobStatus) => {
+							if (
+								jobStatus.status === JobStatusType.Success &&
+								this.options.doFinished
+							) {
+								return resolveAsyncReturn(
+									this.options.doFinished(jobStatus, jobData)
+								);
+							} else if (jobStatus.status === JobStatusType.Failed) {
+								return resolveAsyncReturn(
+									this.options.doFailed?.(jobStatus, jobData) ?? jobStatus,
+									true
+								);
+							} else {
+								return of(jobStatus as any);
+							}
+						})
+					);
+				}),
+				finalize(() => {
+					this._jobStateSubject.complete();
+					this._jobStatusSubject.complete();
+				})
+			);
+		}).pipe(takeUntil(this._destroySubject));
+	}
+
+	destroy() {
+		if (!this.destroyed) {
+			this.state = JobStateType.Destroyed;
+			this._destroySubject.next();
+			this._destroySubject.complete();
+
+			this._jobStateSubject.complete();
+			this._jobStatusSubject.complete();
+		}
+	}
+
+	clone() {
+		return new RxPoolingTask(this.options);
+	}
+
+	private _checkDestroyed() {
+		if (this.destroyed) {
+			throw new Error('Task was destroyed');
+		}
+	}
+
+	private _observeJob(
+		jobData: T,
+		initialCheckInterval: number,
+		checkInterval: number
+	) {
+		return new Observable<JobStatusInfo<I>>((subscriber) => {
+			let periodicSubscription: Subscription;
+
+			subscriber.add(() => {
+				periodicSubscription?.unsubscribe();
+			});
+
+			let firstExecution = true;
+			let interval: number;
+
+			const checkPeriodicStatus = () => {
+				if (firstExecution) {
+					firstExecution = false;
+					interval = initialCheckInterval;
+				} else {
+					interval = checkInterval;
+				}
+
+				periodicSubscription?.unsubscribe();
+				periodicSubscription = resolveAsyncReturn(
+					this.options.checkJobFn(jobData)
+				)
+					.pipe(
+						subscribeOn(asyncScheduler, interval),
+						retry(this.options.retryTimes ?? 0)
+					)
+					.subscribe({
+						next: (res) => {
+							this._jobStatusSubject.next(res);
+							subscriber.next(res);
+
+							if (
+								res?.status === JobStatusType.Success ||
+								res?.status === JobStatusType.Failed
+							) {
+								this.state = JobStateType.Finished;
+
+								subscriber.complete();
+							} else {
+								checkPeriodicStatus();
+							}
+						},
+						error: (response) => {
+							this._jobStatusSubject.next({
+								status: JobStatusType.Failed,
+								error: response,
+							});
+
+							this.state = JobStateType.Finished;
+
+							subscriber.error(response);
+						},
+					});
+			};
+
+			checkPeriodicStatus();
+		});
+	}
+}
---
+++ tests/rx-pooling.spec.ts
@@ -0,0 +1,124 @@
+import { Observable, zip } from 'rxjs';
+import { delay, filter, first, map } from 'rxjs/operators';
+import { JobStatusInfo, JobStatusType, RxPoolingTask } from '../src/pooling-task';
+
+type JobFake = {id: number; error?: boolean; success?: boolean; running?: boolean};
+
+const jobsMap = new Map<number, JobFake>();
+let JOB_ID_CURRENT = 0;
+
+function createJob(time: number, shouldError = false) {
+  return new Observable<number>(subscriber => {
+    const jobId = ++JOB_ID_CURRENT;
+    const job: JobFake = {id: jobId, running: true};
+    jobsMap.set(jobId, job);
+
+    setTimeout(() => {
+      job.running = false;
+      job.success = !shouldError
+      job.error = shouldError
+    }, time);
+
+    subscriber.next(jobId);
+    subscriber.complete();
+  }).pipe(delay(100))
+}
+
+function checkJob(jobId: number) {
+  return new Observable<JobFake>(subscriber => {
+    const job = jobsMap.get(jobId)
+
+    subscriber.next(job);
+    subscriber.complete();
+  }).pipe(delay(100))
+}
+
+describe('RxPoolingTask', () => {
+  test('Should Job Success', (done) => {
+    const pooling = new RxPoolingTask({
+      checkInterval: 50,
+      jobFactory: () => {
+        return createJob(1000, false)
+      },
+      checkJobFn: jobId => {
+        return checkJob(jobId).pipe(map(res => {
+          let info: JobStatusInfo<JobFake>;
+
+          if (res.error) {
+            info = {
+              status: JobStatusType.Failed,
+              error: res
+            }
+          } else if (res.success) {
+            info = {
+              status: JobStatusType.Success,
+              data: res
+            }
+          } else {
+            info = {
+              status: JobStatusType.Running,
+              data: res
+            }
+          }
+
+          return info;
+        }))
+      },
+      doFinished: info => {
+        return info.data;
+      }
+    });
+
+    pooling.submit().subscribe((job) => {
+      expect(job?.success).toBe(true);
+
+      done();
+    })
+    
+  });
+
+  test('Should Job Failed', (done) => {
+    const pooling = new RxPoolingTask({
+      checkInterval: 50,
+      jobFactory: () => {
+        return createJob(1000, true)
+      },
+      checkJobFn: jobId => {
+        return checkJob(jobId).pipe(map(res => {
+          let info: JobStatusInfo<JobFake>;
+
+          if (res.error) {
+            info = {
+              status: JobStatusType.Failed,
+              error: res
+            }
+          } else if (res.success) {
+            info = {
+              status: JobStatusType.Success,
+              data: res
+            }
+          } else {
+            info = {
+              status: JobStatusType.Running,
+              data: res
+            }
+          }
+
+          return info;
+        }))
+      },
+      doFinished: info => {
+        return info.data;
+      },
+    });
+
+    pooling.submit().subscribe({
+      error: (job) => {
+        expect(job?.error).toBe(true);
+  
+        done();
+      }
+    })
+    
+  });
+});
